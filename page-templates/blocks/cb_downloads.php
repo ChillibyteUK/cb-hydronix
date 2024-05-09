51cfd28072fc986fa831d6aca627c130bd475a62
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-05-09 09:31:05 +0100

    Update cb_downloads.php

--- page-templates/blocks/cb_downloads.php
+++ page-templates/blocks/cb_downloads.php
@@ -106,7 +106,7 @@ foreach ($dsubs as $d) {
     var_dump($d);
     echo '-->';
 
-    $checkbox_value = get_field('is_current', 'term_' . $d->ID);
+    $checkbox_value = get_field('is_current', 'term_' . $d->term_id);
 
     if ($checkbox_value == 'Yes') {
         $liveProducts[$d->slug] = $d->name;
