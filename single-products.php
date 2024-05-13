c7a22b33a8aa1cbbdcc8330054a24091f6ec8b1f
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-05-13 13:35:44 +0100

    Update single-products.php

--- single-products.php
+++ single-products.php
@@ -727,7 +727,7 @@ $sku = get_field('product_code') == '' ? get_the_title() : get_field('product_co
     require_once CB_THEME_DIR . '/page-templates/blocks/cb_support_cta.php';
 
 
-ob_start();
+// ob_start();
 $hasContent = 0;
 ?>
     <section id="docs" class="resources py-5">
@@ -794,9 +794,10 @@ do_action('wpml_switch_language', $current_lang);
     $tabs = array();
 
     // // get latest version of product
-    $slug = get_field('latest_slug')->name ?? $post->post_name;
+    $slug = get_field('latest_slug');
 
     var_dump($slug);
+
     // get slugs from docprod matching slug-nn
     // if there are none, use the slug as is.
     // if there is more than one, find the one with 'latest' checked.
