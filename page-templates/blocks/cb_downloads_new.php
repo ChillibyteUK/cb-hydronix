7d2d6febe1c0aeccb1b10abbf46e7445cbdffc25
Matthew Pont <matthew.pont@gmail.com>
2024-10-31 10:18:08 +0000

    Update cb_downloads_new.php

--- page-templates/blocks/cb_downloads_new.php
+++ page-templates/blocks/cb_downloads_new.php
@@ -265,7 +265,39 @@ foreach ($dlangs as $dl) {
 
 
 <?php
-$categories = wp_list_categories( 'echo=0&taxonomy=docprod&orderby=count&hierarchical=true' );
+/**
+ * Recursively get taxonomy and its children
+ *
+ * @param string $taxonomy
+ * @param int $parent - parent term id
+ * @return array
+ */
+function get_taxonomy_hierarchy( $taxonomy, $parent = 0 ) {
+    // only 1 taxonomy
+    $taxonomy = is_array( $taxonomy ) ? array_shift( $taxonomy ) : $taxonomy;
+
+    // get all direct decendants of the $parent
+    $terms = get_terms( $taxonomy, array( 'parent' => $parent ) );
+
+    // prepare a new array.  these are the children of $parent
+    // we'll ultimately copy all the $terms into this new array, but only after they
+    // find their own children
+    $children = array();
+
+    // go through all the direct decendants of $parent, and gather their children
+    foreach ( $terms as $term ){
+        // recurse to get the direct decendants of "this" term
+        $term->children = get_taxonomy_hierarchy( $taxonomy, $term->term_id );
+
+        // add the term to our new array
+        $children[ $term->term_id ] = $term;
+    }
+
+    // send the results back to the caller
+    return $children;
+}
+
+$categories = get_taxonomy_hierarchy( 'docprod' );
 echo "<pre>";
 print_r( json_encode( $categories ) );
 echo "</pre>";
