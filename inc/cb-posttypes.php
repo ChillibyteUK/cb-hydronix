6eb25f07176649e8b45e875ce99e10fd3290da7a
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-26 10:52:05 +0000

    event tx test

--- inc/cb-posttypes.php
+++ inc/cb-posttypes.php
@@ -2,142 +2,145 @@
 
 function cb_register_post_types() {
 
-$labels = [
-    "name" => __( "Products", "cb-hydronix" ),
-    "singular_name" => __( "Product", "cb-hydronix" ),
-];
+    $labels = [
+        "name" => __( "Products", "cb-hydronix" ),
+        "singular_name" => __( "Product", "cb-hydronix" ),
+    ];
 
-$args = [
-    "label" => __( "Products", "cb-hydronix" ),
-    "labels" => $labels,
-    "description" => "",
-    "public" => true,
-    "publicly_queryable" => true,
-    "show_ui" => true,
-    "show_in_rest" => true,
-    "rest_base" => "",
-    "rest_controller_class" => "WP_REST_Posts_Controller",
-    "has_archive" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "delete_with_user" => false,
-    "exclude_from_search" => false,
-    "capability_type" => "post",
-    "map_meta_cap" => true,
-    "hierarchical" => false,
-    "rewrite" => [ "slug" => "products", "with_front" => false ],
-    "query_var" => true,
-    "supports" => [ "title",  "thumbnail" ],
-    "show_in_graphql" => false,
-];
+    $args = [
+        "label" => __( "Products", "cb-hydronix" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "delete_with_user" => false,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => [ "slug" => "products", "with_front" => false ],
+        "query_var" => true,
+        "supports" => [ "title",  "thumbnail" ],
+        "show_in_graphql" => false,
+    ];
 
-register_post_type( "products", $args );
+    register_post_type( "products", $args );
 
-$labels = [
-    "name" => __( "Accessories", "cb-hydronix" ),
-    "singular_name" => __( "Accessory", "cb-hydronix" ),
-];
+    $labels = [
+        "name" => __( "Accessories", "cb-hydronix" ),
+        "singular_name" => __( "Accessory", "cb-hydronix" ),
+    ];
 
-$args = [
-    "label" => __( "Accessories", "cb-hydronix" ),
-    "labels" => $labels,
-    "description" => "",
-    "public" => true,
-    "publicly_queryable" => true,
-    "show_ui" => true,
-    "show_in_rest" => true,
-    "rest_base" => "",
-    "rest_controller_class" => "WP_REST_Posts_Controller",
-    "has_archive" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "delete_with_user" => false,
-    "exclude_from_search" => true,
-    "capability_type" => "post",
-    "map_meta_cap" => true,
-    "hierarchical" => false,
-    "rewrite" => false ,
-    "query_var" => true,
-    "supports" => [ "title", "thumbnail" ],
-    "show_in_graphql" => false,
-];
+    $args = [
+        "label" => __( "Accessories", "cb-hydronix" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "delete_with_user" => false,
+        "exclude_from_search" => true,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => false ,
+        "query_var" => true,
+        "supports" => [ "title", "thumbnail" ],
+        "show_in_graphql" => false,
+    ];
 
-register_post_type( "accessories", $args );
+    register_post_type( "accessories", $args );
 
-$labels = [
-    "name" => __( "News", "cb-hydronix" ),
-    "singular_name" => __( "News", "cb-hydronix" ),
-];
+    $labels = [
+        "name" => __( "News", "cb-hydronix" ),
+        "singular_name" => __( "News", "cb-hydronix" ),
+    ];
 
-$args = [
-    "label" => __( "News", "cb-hydronix" ),
-    "labels" => $labels,
-    "description" => "",
-    "public" => true,
-    "publicly_queryable" => true,
-    "show_ui" => true,
-    "show_in_rest" => true,
-    "rest_base" => "",
-    "rest_controller_class" => "WP_REST_Posts_Controller",
-    "has_archive" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "delete_with_user" => false,
-    "exclude_from_search" => false,
-    "capability_type" => "post",
-    "map_meta_cap" => true,
-    "hierarchical" => false,
-    "rewrite" => [ "slug" => "resources/news", "with_front" => false ],
-    "query_var" => true,
-    "supports" => [ "title", "editor", "thumbnail" ],
-    "show_in_graphql" => false,
-];
+    $args = [
+        "label" => __( "News", "cb-hydronix" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "delete_with_user" => false,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => [ "slug" => "resources/news", "with_front" => false ],
+        "query_var" => true,
+        "supports" => [ "title", "editor", "thumbnail" ],
+        "show_in_graphql" => false,
+    ];
 
-$translated_slug = function_exists('icl_translate')
-? icl_translate('Custom Post Type Slugs', 'events_archive_slug', 'resources/events')
-: 'resources/events';
+    if (function_exists('wpml_register_single_string')) {
+        do_action('wpml_register_single_string', 'Custom Post Type Slugs', 'events_archive_slug', 'resources/events');
+    }
 
-$labels = [
-    "name" => __( "Events", "cb-hydronix" ),
-    "singular_name" => __( "Event", "cb-hydronix" ),
-];
+    $translated_slug = function_exists('icl_translate')
+    ? icl_translate('Custom Post Type Slugs', 'events_archive_slug', 'resources/events')
+    : 'resources/events';
 
-$args = [
-    "label" => __( "Events", "cb-hydronix" ),
-    "labels" => $labels,
-    "description" => "",
-    "public" => true,
-    "publicly_queryable" => true,
-    "show_ui" => true,
-    "show_in_rest" => true,
-    "rest_base" => "",
-    "rest_controller_class" => "WP_REST_Posts_Controller",
-    "has_archive" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "delete_with_user" => false,
-    "exclude_from_search" => false,
-    "capability_type" => "post",
-    "map_meta_cap" => true,
-    "hierarchical" => false,
-    "rewrite" => [ "slug" => $translated_slug, "with_front" => false ],
-    "query_var" => true,
-    "supports" => [ "title", "thumbnail" ],
-    "show_in_graphql" => false,
-    'taxonomies' => array('post_tag')
-];
+    $labels = [
+        "name" => __( "Events", "cb-hydronix" ),
+        "singular_name" => __( "Event", "cb-hydronix" ),
+    ];
 
-register_post_type( "events", $args );
+    $args = [
+        "label" => __( "Events", "cb-hydronix" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "delete_with_user" => false,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => [ "slug" => $translated_slug, "with_front" => false ],
+        "query_var" => true,
+        "supports" => [ "title", "thumbnail" ],
+        "show_in_graphql" => false,
+        'taxonomies' => array('post_tag')
+    ];
+
+    register_post_type( "events", $args );
+
+    if (!get_option('custom_events_rewrite_flushed')) {
+        flush_rewrite_rules();
+        update_option('custom_events_rewrite_flushed', true);
+    }
 
 }
 add_action( 'init', 'cb_register_post_types' );
 
-// bag of shite WPML
-add_action('init', function () {
-    if (function_exists('wpml_register_single_string')) {
-        do_action('wpml_register_single_string', 'Custom Post Type Slugs', 'events_archive_slug', 'resources/events');
-    }
-});
 
 add_action( 'after_switch_theme', 'cb_rewrite_flush' );
 function cb_rewrite_flush() {
