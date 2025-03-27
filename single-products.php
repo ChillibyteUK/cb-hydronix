04cb939e425cf60d4d1e9fb0b920a1a8f035cf76
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-27 11:48:34 +0000

    accessory images

--- single-products.php
+++ single-products.php
@@ -576,7 +576,7 @@ if (get_field('accessories')) {
                         <ul class="cols-lg-3 nopad">
                             <?php
             foreach (get_field('accessories') as $i) {
-                if (has_term('option','atype',$i)) {
+                if (get_the_post_thumbnail($i)) {
                     $img = get_the_post_thumbnail_url($i, 'full');
                     echo '<li><a href="' . $img . '" data-lightbox="gallery">' . get_the_title($i) . '</a></li>';
                 // echo '<li>' . get_the_title($i) . '</li>';
