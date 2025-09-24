db1a4d56f56a2108bbf2fe3694771dbff9f1b02c
ChillibyteDan <dan@chillibyte.co.uk>
2025-09-24 14:09:47 +0100

    single cement related

--- single-cement_results.php
+++ single-cement_results.php
@@ -348,6 +348,61 @@ echo $header;
 				</div>
 			</div>
 		</section>
+		<?php
+		$id1 = 1187;
+		$id2 = 1186;
+		?>
+		<style>
+			.single_post__card {
+				background-color: white;
+				box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+				text-decoration: none;
+				color: #3c3c3c;
+				transition: all 0.3s ease;
+			}
+		</style>
+		<!-- two posts -->
+		<section
+			class="single_post py-5">
+			<div class="container-xl px-0">
+				<h2 class="mb-4">Related Content</h2>
+				<div class=" row g-4 mb-4">
+					<div class="col-lg-6">
+						<div class="single_post__card h-100">
+							<div class="row">
+								<div class="col-md-3">
+									<?= get_the_post_thumbnail( $id1 ); ?>
+								</div>
+								<div class="col-md-9 p-3">
+									<h3><?= get_the_title( $id1 ); ?></h3>
+									<p><?= wp_trim_words( get_the_content( null, false, $id1 ), 30 ); ?>
+									</p>
+									<a href="<?= get_the_permalink( $id1 ); ?>"
+										class="btn btn--orange ms-md-auto me-md-4"><?= __( 'Read more', 'cb-hydronix' ); ?></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="single_post__card h-100">
+							<div class="row">
+								<div class="col-md-3">
+									<?= get_the_post_thumbnail( $id2 ); ?>
+								</div>
+								<div class="col-md-9 p-3">
+									<h3><?= get_the_title( $id2 ); ?></h3>
+									<p><?= wp_trim_words( get_the_content( null, false, $id2 ), 30 ); ?>
+									</p>
+									<a href="<?= get_the_permalink( $id2 ); ?>"
+										class="btn btn--orange ms-md-auto me-md-4"><?= __( 'Read more', 'cb-hydronix' ); ?></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
 		    <?php
         }
     }
